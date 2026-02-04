bb299d490a4b7c4124e88a6062c8193675f1db60
Yousaf <dilshersaiqadilsher@gmail.com>
2026-02-04 18:01:55 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const CACHE_NAME = "studybuddy-v2";
+
 const FILES = [
   "./",
   "./index.html",
@@ -7,14 +8,32 @@ const FILES = [
   "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(FILES))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then((res) => {
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
